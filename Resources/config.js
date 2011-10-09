b12945639da2c7ef9e9f79f111953c15fd6c3c5d
Luis Elizondo <lelizondo@gmail.com>
2011-10-09 00:49:24 -0500

    get-node is working

--- Resources/config.js
+++ Resources/config.js
@@ -4,5 +4,5 @@
  * everywhere we need it.
  */
 
-// Set this to the base path of your rest server (no trailing slash)
-var SITE_PATH = 'http://192.168.1.206:8888/drupanium/api/rest';
+// Set this to the base path of your rest server (add trailing slash)
+var SITE_PATH = 'http://192.168.1.206:8888/drupanium/api/rest/';
--- Resources/includes/get-node.js
+++ Resources/includes/get-node.js
@@ -1,44 +1,105 @@
+/**
+ * Remember that in the debug process we can always use:
+ * Ti.API.info(foo);
+ * to log something to the console
+ */
+
+// Include our config file
 Ti.include('../config.js');
 
-// Define the current window in a variable
+// Define the variable win to contain the current window
 var win = Ti.UI.currentWindow;
 
-var url = SITE_PATH + '/node/' + 1 + '.json';
+// Create the scrollview to contain the view
+var scrollView = Titanium.UI.createScrollView({
+	contentWidth:'auto',
+	contentHeight:'auto',
+	showVerticalScrollIndicator:true,
+	showHorizontalScrollIndicator:true,
+	top: 0,
+});
 
-//First, you'll want to check the user can access the web:
-if (Titanium.Network.online == true) {
- var xhr = Titanium.Network.createHTTPClient();
- xhr.open('GET',url);
- xhr.setRequestHeader('Content-Type','application/json; charset=utf-8');
- xhr.send();
- 
- xhr.onload = function requestReceived(e) {
-	   var statusCode = xhr.status;
-	   if (statusCode == 200) {
-	     var response = xhr.responseText;
-	     var result = JSON.parse(response);
-	     
-	  // Message
-	     var nodeTitle = Ti.UI.createLabel({
-				text: result.title,
-				color:'#ff0000',
-				textAlign:'left',
-				font:{fontSize:16, fontWeight:'normal'},
-				top: 0,
-				left: 28,
-				right: 28,
-				touchEnabled: true,
-				height: "auto"
-	     });
-	     
-	     
-	     
-	     scrollView.add(view);
-	     view.add(nodeTitle);
-	     view.add(nodeBody);
-	     
-	     win.add(scrollView);
-	   }
- }
- 
+// Create a view, we'll be adding our data to this view
+var view = Ti.UI.createView({
+	backgroundColor:'#fff',
+	width:300,
+	height:2000,
+	top: 0,
+});
+
+// Add our view to the scrollview
+scrollView.add(view);
+
+// Add our scrollview to the window
+win.add(scrollView);
+
+// Define the url which contains the full url
+// in this case, we'll connecting to http://example.com/api/rest/node/1.json
+var url = SITE_PATH + 'node/1' + '.json';
+
+// Create a conection inside the variable connection
+var connection = Titanium.Network.createHTTPClient();
+
+// Open the connection
+connection.open("GET",url);
+
+// Send the connection
+connection.send();
+
+// When the connection loads we do:
+connection.onload = function() {
+	// Save the status of the connection in a variable
+	// this will be used to see if we have a connection (200) or not
+	var statusCode = connection.status;
+	
+	// Check if we have a connection
+	if(statusCode == 200) {
+		// Save the responseText from the connection in the response variable
+		var response = connection.responseText;
+		
+		// Parse (build data structure) the JSON response into an object (data)
+		var data = JSON.parse(response);
+		
+		// Create a label for the node title
+		var nodeTitle = Ti.UI.createLabel({
+			// The text of the label will be the node title (data.title)
+			text: data.title,
+			color:'#000',
+			textAlign:'left',
+			font:{fontSize:16, fontWeight:'bold'},
+			top:10,
+			height:18
+		});
+		
+		// Create a label for the node body
+		var nodeBody = Ti.UI.createLabel({
+			// Because D7 uses an object for the body itself including the language
+			text: data.body.und[0].value,
+			color:'#000',
+			textAlign:'left',
+			font:{fontSize:14, fontWeight:'normal'},
+			top: 30,
+			height: 2000,
+		});
+		
+		// Add both nodeTitle and nodeBody labels to our view
+		view.add(nodeTitle);
+		view.add(nodeBody);
+	}
+	else {
+		// Create a label for the node title
+		var errorMessage = Ti.UI.createLabel({
+			// The text of the label will be the node title (data.title)
+			text: "Please check your internet connection.",
+			color:'#000',
+			textAlign:'left',
+			font:{fontSize:24, fontWeight:'bold'},
+			top:25,
+			left:15,
+			height:18
+		});
+		
+		// Add the error message to the window
+		win.add(errorMessage);
+	}
 }
