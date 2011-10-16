aafd97704f35775e35b619c276f1418df994653e
Luis Elizondo <lelizondo@gmail.com>
2011-10-16 04:13:29 -0500

    view-all-content now works loading the respective node

--- Resources/includes/view-all-content.js
+++ Resources/includes/view-all-content.js
@@ -62,20 +62,90 @@ connection.onload = function() {
 		var response = connection.responseText;
 		
 		// Parse (build data structure) the JSON response into an object (data)
-		var data = JSON.parse(response);
-		
-		// Since the data is already an object and titanium request a data array
-		// we just pass it
-		// also see this example:
-		// var data = [{title:"Row 1"},{title:"Row 2"}];
-		// var table = Titanium.UI.createTableView({data:data});
-		// view.add(table);
-		var table = Titanium.UI.createTableView({data:data});
+		var result = JSON.parse(response);
 		
 		/**
-		 * how to make that when we select a title it goes
-		 * to a new window and open a the complete node
+		 * Create a new array "results"
+		 * This is necessary because we need to create an object
+		 * to send to the Table we're creating with the results
+		 * the table will have the title and the nid of every result
+		 * and we'll use the nid to move to another window when we click
+		 * on it. 
 		 */
+		var results = new Array();
+		
+		// Start loop
+		for(var key in result) {
+			// Create the data variable and hold every result
+			var data = result[key];
+			
+			/**
+			 * To see how the array is built by Services in Drupal
+			 * go to drupanium debug and use the views debug page
+			 * you'll see that the array is something like:
+			 * 
+			 * 0 => array(
+			 * 	title => some title
+			 * 	date => some date
+			 *  user => the user uid
+			 *  type => the node type
+			 *  nid => the node nid
+			 *  vid => the node vid
+			 * )
+			 */
+			
+			// Uncomment the next line to see the full object in JSON format 
+			// alert(result[key]); 
+			
+			// Uncomment the next line to see an example of how to get the title of every result
+			// alert(data.title);
+			
+			/**
+			 * We start adding the results one by one to our array "results"
+			 * it consists of title, nid and the property hasChild 
+			 * in title we get the node title with data.title
+			 * in nid we save the node nid with data.nid (we walk the array)
+			 */
+			results[key] = {title: data.title, hasChild:true, nid:data.nid};
+		}
+		
+		// Create a new table to hold our results
+		// We tell Titanium to use our array results as the Property "data"
+		// See http://developer.appcelerator.com/apidoc/mobile/latest/Titanium.UI.TableView-object
+		// Specially the properties
+		var table = Titanium.UI.createTableView({data:results});
+		
+		// add a listener for click to the table
+		// so every row is clickable 
+		table.addEventListener('click',function(e) {
+			
+			// Define a new Window "nodeWindow"
+            var nodeWindow = Titanium.UI.createWindow({
+            	// the window is not here, but in the file get-node-by-nid.js
+            	// so we load it
+            	url:'get-node-by-nid.js',
+            	
+            	// define some basic properties
+            	backgroundColor:'#fff',
+            	
+            	// Define the title of our new window using the node title
+            	// e.rowData contains the information we defined when we passed it
+            	// to Titanium.UI.createTableView using the property "data"
+            	// so e.rowData.title = data.title for each of the rows in the table
+            	title:e.rowData.title,
+            	
+            	// The same for the nid
+            	// We send the nid as an property in this window and the 
+            	// get-node-by-nid file will recognize it and use it
+            	nid:e.rowData.nid,
+            	
+            	// a boolean indicating if the view should receive touch events (true, default) or forward them to peers (false)
+            	touchEnabled: true
+            });
+            
+            // order the app to open the nodeWindow window in the current Tab
+            Titanium.UI.currentTab.open(nodeWindow,{animated:true});
+        });
 		
 		// add our table to the view
 		view.add(table);
